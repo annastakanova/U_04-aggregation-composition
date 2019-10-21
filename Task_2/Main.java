db1e16d6af0b68033cf98f5ef7ecade7bfdb3f72
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-10-21 23:56:15 +0300

    вызов через геттеры

--- Task_2/Main.java
+++ Task_2/Main.java
@@ -2,25 +2,28 @@ package by.epam.Unit04;
 //Создать объект класса Текст, используя классы Предложение, Слово.
 // Методы: дополнить текст, вывести на консоль текст, заголовок текста.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Sentence sentence = new Sentence();
-        sentence.addWord(new Word("Создать объект класса текст,"));
-        sentence.addWord(new Word("используя"));
-        sentence.addWord(new Word("классы:"));
-        sentence.addWord(new Word("предложение,"));
-        sentence.addWord(new Word("слово."));
-
+        List<Sentence> titleSentences = new ArrayList<Sentence>();
         Sentence title = new Sentence();
-        title.addWord(new Word("Классы."));
+        title.addWord(new Word("Заголовок предложения."));
+        titleSentences.add(title);
+
+        List<Sentence> textSentences = new ArrayList<Sentence>();
+        Sentence text1 = new Sentence();
+        Sentence text2 = new Sentence();
+        text1.addWord(new Word("Текст предложения1."));
+        text2.addWord(new Word("Текст предложения2."));
+        textSentences.add(text1);
+        textSentences.add(text2);
 
         Text tl = new Text();
-        tl.addTitle(title);
-        tl.addSentence(sentence);
+        tl.addTitle(titleSentences);
+        tl.addSentence(textSentences);
 
         System.out.print("Заголовок: ");
         print(tl.getTitle());
